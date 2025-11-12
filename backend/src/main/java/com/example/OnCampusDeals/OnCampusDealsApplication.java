2a5208c1e0fec4f5172543e53cc117984b9edd10
Krish Patel <krishbipinpatel@gmail.com>
2025-11-12 23:54:33 +0530

    Added the backend framework

---
+++ backend/src/main/java/com/example/OnCampusDeals/OnCampusDealsApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnCampusDeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnCampusDealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnCampusDealsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/OnCampusDeals/OnCampusDealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnCampusDeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnCampusDealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
